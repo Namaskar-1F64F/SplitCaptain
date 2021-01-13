cad88499937cf34a0ca0f4c94e988cefb32c9e88
russell <russell@deephire.com>
2021-01-13 15:35:24 -0600

    select who paid dynamically

---
+++ src/excursion/Crewmate.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let name;
+</script>
+
+<div class="inline-block">
+    <label
+        for="description"
+        class="block text-sm font-medium text-gray-700">{name}</label>
+    <div class="mt-1">
+        <input type="checkbox" name="checkbox" id={`${name}`} />
+    </div>
+</div>
--- src/excursion/Excursion.svelte
+++ src/excursion/Excursion.svelte
@@ -1,5 +1,6 @@
 <script>
-  import { excursion } from "../store";
+  import { excursion, voyage } from "../store";
+  import  Crewmate  from "./Crewmate.svelte";
   import { Collection } from "sveltefire";
   const addToArray = (provisionRef) => {
     provisionRef.add({});
@@ -15,91 +16,94 @@
 
 <div>
   <Collection
-    path={$excursion.collection('provisions')}
-    let:data={excursionsData}
-    let:ref>
-    <form on:submit|preventDefault={() => addToArray(ref)}>
-      <ul class="divide-y divide-gray-200">
-        {#each excursionsData as item}
-          <li class="px-4 py-4 sm:px-6">
-            <div>
-              <label
-                for="price"
-                class="block text-sm font-medium text-gray-700">Price</label>
-              <div class="mt-1 relative rounded-md shadow-sm">
-                <div
-                  class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <span class="text-gray-500 sm:text-sm"> $ </span>
-                </div>
-                <input
-                  value={item.price || ''}
-                  on:change={({ target: { value } }) => {
-                    ref.doc(item.id).update({ price: value });
-                  }}
-                  type="text"
-                  name="price"
-                  id={`price-${item.id}`}
-                  class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
-                  placeholder="0.00" />
-                <div class="absolute inset-y-0 right-0 flex items-center">
-                  <label for="currency" class="sr-only">Currency</label>
-                  <select
-                    value={item.currency}
-                    on:blur={({ target: { value } }) => {
-                      ref.doc(item.id).update({ currency: value });
-                    }}
-                    id={`currency-${item.id}`}
-                    name="currency"
-                    class="focus:ring-indigo-500 focus:border-indigo-500 h-full py-0 pl-2 pr-7 border-transparent bg-transparent text-gray-500 sm:text-sm rounded-md">
-                    <option>USD</option>
-                    <option>MX</option>
-                  </select>
-                </div>
-              </div>
-
+    path={$voyage.collection('crewmates')}
+    let:data={crewmates}
+    let:ref={crewmatesRef}>
+    <Collection
+      path={$excursion.collection('provisions')}
+      let:data={provisions}
+      let:ref={excursionRef}>
+      <form on:submit|preventDefault={() => addToArray(excursionRef)}>
+        Who Paid ? 
+        {#each crewmates as crewmate}
+        <Crewmate name={crewmate.name}/>
+      {/each}
+        <ul class="divide-y divide-gray-200">
+          {#each provisions as provision}
+            <li class="px-4 py-4 sm:px-6">
               <div>
                 <label
-                  for="description"
-                  class="block text-sm font-medium text-gray-700">Description</label>
-                <div class="mt-1">
+                  for="price"
+                  class="block text-sm font-medium text-gray-700">Price</label>
+                <div class="mt-1 relative rounded-md shadow-sm">
+                  <div
+                    class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                    <span class="text-gray-500 sm:text-sm"> $ </span>
+                  </div>
                   <input
-                    value={item.description || ''}
+                    value={provision.price || ''}
                     on:change={({ target: { value } }) => {
-                      ref.doc(item.id).update({ description: value });
+                      excursionRef.doc(provision.id).update({ price: value });
                     }}
                     type="text"
-                    name="description"
-                    id={`description-${item.id}`}
-                    class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                    placeholder="Yarg " />
+                    name="price"
+                    id={`price-${provision.id}`}
+                    class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
+                    placeholder="0.00" />
+                  <div class="absolute inset-y-0 right-0 flex items-center">
+                    <label for="currency" class="sr-only">Currency</label>
+                    <select
+                      value={provision.currency}
+                      on:blur={({ target: { value } }) => {
+                        excursionRef.doc(provision.id).update({ currency: value });
+                      }}
+                      id={`currency-${provision.id}`}
+                      name="currency"
+                      class="focus:ring-indigo-500 focus:border-indigo-500 h-full py-0 pl-2 pr-7 border-transparent bg-transparent text-gray-500 sm:text-sm rounded-md">
+                      <option>USD</option>
+                      <option>MX</option>
+                    </select>
+                  </div>
+                </div>
+
+                <div>
+                  <label
+                    for="description"
+                    class="block text-sm font-medium text-gray-700">Description</label>
+                  <div class="mt-1">
+                    <input
+                      value={provision.description || ''}
+                      on:change={({ target: { value } }) => {
+                        excursionRef
+                          .doc(provision.id)
+                          .update({ description: value });
+                      }}
+                      type="text"
+                      name="description"
+                      id={`description-${provision.id}`}
+                      class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                      placeholder="Yarg " />
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <div class="inline-block">
-              <label
-                for="description"
-                class="block text-sm font-medium text-gray-700">Kyle</label>
-              <div class="mt-1">
-                <input
-                  bind:value={item.price}
-                  type="checkbox"
-                  name="checkbox"
-                  id={`person-${item.id}`} />
+
+              {#each crewmates as crewmate}
+                <Crewmate name={crewmate.name}/>
+              {/each}
+              <div
+                class=" bg-red-600"
+                on:click={() => deleteFromArray(excursionRef, provision.id)}>
+                Delete Provision
               </div>
-            </div>
-            <div
-              class=" bg-red-600"
-              on:click={() => deleteFromArray(ref, item.id)}>
-              Delete Provision
-            </div>
-          </li>
-        {/each}
-      </ul>
-      <button type="submit" />
-    </form>
-    <div class=" bg-green-400" on:click={() => addToArray(ref)}>
-      Add provision
-    </div>
+            </li>
+          {/each}
+        </ul>
+        <button type="submit" />
+      </form>
+      <div class=" bg-green-400" on:click={() => addToArray(excursionRef)}>
+        Add provision
+      </div>
+    </Collection>
   </Collection>
 </div>
