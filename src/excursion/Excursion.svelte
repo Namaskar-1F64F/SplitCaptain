ff5f483dea2b5e6a75aaa81ad44e5da9964f6d10
russell <russell@deephire.com>
2021-01-14 08:56:49 -0600

    grab keys for the provisions

--- src/excursion/Excursion.svelte
+++ src/excursion/Excursion.svelte
@@ -20,7 +20,7 @@
     let:ref>
     <form on:submit|preventDefault={() => addToArray(ref)}>
       <ul class="divide-y divide-gray-200">
-        {#each excursionsData as item}
+        {#each excursionsData as { price, id, currency, description } (id)}
           <li class="px-4 py-4 sm:px-6">
             <div>
               <label
@@ -32,23 +32,23 @@
                   <span class="text-gray-500 sm:text-sm"> $ </span>
                 </div>
                 <input
-                  value={item.price || ''}
+                  value={price || ''}
                   on:change={({ target: { value } }) => {
-                    ref.doc(item.id).update({ price: value });
+                    ref.doc(id).update({ price: value });
                   }}
                   type="text"
                   name="price"
-                  id={`price-${item.id}`}
+                  id={`price-${id}`}
                   class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
                   placeholder="0.00" />
                 <div class="absolute inset-y-0 right-0 flex items-center">
                   <label for="currency" class="sr-only">Currency</label>
                   <select
-                    value={item.currency}
+                    value={currency}
                     on:blur={({ target: { value } }) => {
-                      ref.doc(item.id).update({ currency: value });
+                      ref.doc(id).update({ currency: value });
                     }}
-                    id={`currency-${item.id}`}
+                    id={`currency-${id}`}
                     name="currency"
                     class="focus:ring-indigo-500 focus:border-indigo-500 h-full py-0 pl-2 pr-7 border-transparent bg-transparent text-gray-500 sm:text-sm rounded-md">
                     <option>USD</option>
@@ -63,13 +63,13 @@
                   class="block text-sm font-medium text-gray-700">Description</label>
                 <div class="mt-1">
                   <input
-                    value={item.description || ''}
+                    value={description || ''}
                     on:change={({ target: { value } }) => {
-                      ref.doc(item.id).update({ description: value });
+                      ref.doc(id).update({ description: value });
                     }}
                     type="text"
                     name="description"
-                    id={`description-${item.id}`}
+                    id={`description-${id}`}
                     class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
                     placeholder="Yarg " />
                 </div>
@@ -82,15 +82,15 @@
                 class="block text-sm font-medium text-gray-700">Kyle</label>
               <div class="mt-1">
                 <input
-                  bind:value={item.price}
+                  bind:value={price}
                   type="checkbox"
                   name="checkbox"
-                  id={`person-${item.id}`} />
+                  id={`person-${id}`} />
               </div>
             </div>
             <div
               class=" bg-red-600"
-              on:click={() => deleteFromArray(ref, item.id)}>
+              on:click={() => deleteFromArray(ref, id)}>
               Delete Provision
             </div>
           </li>
