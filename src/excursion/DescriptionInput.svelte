4cedf6c3ac508c2ca1cfc5397c16789f96dadbf7
russell <russell@deephire.com>
2021-01-15 11:15:07 -0600

    add total bill

---
+++ src/excursion/DescriptionInput.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let value;
+  export let title;
+  export let ref;
+  export let field;
+  export let id;
+</script>
+
+<div>
+  <label for="description" class="block text-sm font-medium text-gray-700"
+    >{title}</label
+  >
+  <div class="mt-1">
+    <input
+      value={value || ""}
+      on:change={({ target: { value } }) => {
+        ref.update({ [field]: value });
+      }}
+      type="text"
+      name="description"
+      id={`description-${id}`}
+      class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+      placeholder="What was the item?"
+    />
+  </div>
+</div>
--- src/excursion/Excursion.svelte
+++ src/excursion/Excursion.svelte
@@ -1,7 +1,8 @@
 <script>
   import { excursion, voyage } from "../store";
-  import  Crewmate  from "./Crewmate.svelte";
+  import Crewmate from "./Crewmate.svelte";
   import { Collection } from "sveltefire";
+  import DescriptionInput from "./DescriptionInput.svelte";
   const addToArray = (provisionRef) => {
     provisionRef.add({});
   };
@@ -16,32 +17,39 @@
 
 <div>
   <Collection
-    path={$voyage.collection('crewmates')}
+    path={$voyage.collection("crewmates")}
     let:data={crewmates}
-    let:ref={crewmatesRef}>
+    let:ref={crewmatesRef}
+  >
     <Collection
-      path={$excursion.collection('provisions')}
+      path={$excursion.collection("provisions")}
       let:data={provisions}
-      let:ref={excursionRef}>
+      let:ref={excursionRef}
+    >
+      {`Total bill: ${provisions.reduce(
+        (a, b) => parseInt(a.price) + parseInt(b.price, 0)
+      )}`}
       <form on:submit|preventDefault={() => addToArray(excursionRef)}>
-        Who Paid ? 
-        {#each crewmates as {name, id} (id)}
-        <Crewmate name={name}/>
-      {/each}
+        Who Paid ?
+        {#each crewmates as { name, id } (id)}
+          <Crewmate {name} />
+        {/each}
         <ul class="divide-y divide-gray-200">
-          {#each provisions as {price, id, currency, description} (id)}
+          {#each provisions as { price, id, currency, description } (id)}
             <li class="px-4 py-4 sm:px-6">
               <div>
                 <label
                   for="price"
-                  class="block text-sm font-medium text-gray-700">Price</label>
+                  class="block text-sm font-medium text-gray-700">Price</label
+                >
                 <div class="mt-1 relative rounded-md shadow-sm">
                   <div
-                    class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                    class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none"
+                  >
                     <span class="text-gray-500 sm:text-sm"> $ </span>
                   </div>
                   <input
-                    value={price || ''}
+                    value={price || ""}
                     on:change={({ target: { value } }) => {
                       excursionRef.doc(id).update({ price: value });
                     }}
@@ -49,7 +57,8 @@
                     name="price"
                     id={`price-${id}`}
                     class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
-                    placeholder="0.00" />
+                    placeholder="0.00"
+                  />
                   <div class="absolute inset-y-0 right-0 flex items-center">
                     <label for="currency" class="sr-only">Currency</label>
                     <select
@@ -66,44 +75,30 @@
                   </div>
                 </div>
 
-                <div>
-                  <label
-                    for="description"
-                    class="block text-sm font-medium text-gray-700">Description</label>
-                  <div class="mt-1">
-                    <input
-                      value={description || ''}
-                      on:change={({ target: { value } }) => {
-                        excursionRef
-                          .doc(id)
-                          .update({ description: value });
-                      }}
-                      type="text"
-                      name="description"
-                      id={`description-${id}`}
-                      class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                      placeholder="Yarg " />
-                  </div>
-                </div>
-              </div>
+                <DescriptionInput
+                  title="Description"
+                  field="description"
+                  value={description}
+                  ref={excursionRef.doc(id)}
+                />
 
-
-              {#each crewmates as crewmate}
-                <Crewmate name={crewmate.name}/>
-              {/each}
-              <div
-                class=" bg-red-600"
-                on:click={() => deleteFromArray(excursionRef, id)}>
-                Delete Provision
+                {#each crewmates as crewmate}
+                  <Crewmate name={crewmate.name} />
+                {/each}
+                <button
+                  class=" bg-red-600"
+                  on:click={() => deleteFromArray(excursionRef, id)}>
+                  Delete Provision
+                </button>
               </div>
             </li>
           {/each}
         </ul>
         <button type="submit" />
       </form>
-      <div class=" bg-green-400" on:click={() => addToArray(excursionRef)}>
+      <button class=" bg-green-600" on:click={() => addToArray(excursionRef)}>
         Add provision
-      </div>
+      </button>
     </Collection>
   </Collection>
 </div>
