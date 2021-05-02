5352e573557606493e81593361a485784847a77e
Sam Swindell <swindesr@users.noreply.github.com>
2021-05-02 15:39:15 -0400

    Fix build bug with named slot

--- src/profile/StaticProfile.svelte
+++ src/profile/StaticProfile.svelte
@@ -10,7 +10,9 @@
 <div class="w-32" class:selected>
   <StaticTag {selected} text={name}>
     <Doc path={`/mariners/${id}`} let:data let:ref>
-      <Ship slot="fallback" theme={undefined} shipType={undefined}/>
+      <g slot="fallback">
+        <Ship />
+      </g>
       <Ship theme={data.theme} shipType={data.shipType} />
     </Doc>
   </StaticTag>
